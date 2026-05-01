81e7cec8ddd4d05dd2994224020176cabd3fe707
Marco Di Giulio <m.digiulio@jdk.it>
2026-05-01 15:05:07 +0200

    third-validation-example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import StateLogin from './components/StateLogin.jsx';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Header />
       <main>
         {/* <Signup /> */}
+        {/* <Login /> */}
         <StateLogin />
       </main>
     </>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,8 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 // questo è un modo alternativo per la gestione degli user input in react, ma è meno comune ripsetto all'utilizzo dello state, è più difficile resettare i valori, ed è molto più scomodo quando i form sono lunghi e complessi.
 export default function Login() {
+  const [formIsInvalid, setFormIsInvalid] = useState(false);
 
   const email = useRef();
   const password = useRef();
@@ -13,7 +14,12 @@ export default function Login() {
 
     // reset dei valori del form quando gestiti da ref
     event.target.reset();
-   }
+
+    // esempio di validazione al submit, è molto meno performante rispetto all'utilizzo dello state, ma è molto più semplice da utilizzare rispetto all'utilizzo dello state quando i form sono lunghi e complessi, ma è molto più scomodo da utilizzare rispetto all'utilizzo dello state.
+    const emailIsInvalid = !enteredEmail.includes("@");
+    const passwordIsInvalid = enteredPassword.length < 6;
+    setFormIsInvalid(emailIsInvalid || passwordIsInvalid);
+  };
 
 
   return (
@@ -24,12 +30,14 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          {formIsInvalid && <p className="error">Please enter a valid email.</p>}
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input id="password" type="password" name="password" ref={password} />
         </div>
+        {formIsInvalid && <p className="error">Please enter a valid password (at least 6 characters).</p>}
       </div>
 
       <p className="form-actions">
